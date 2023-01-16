68738ca52680621c8bd760abef403339405f7d46
Gustavo <gustavopchaves@hotmail.com>
2023-01-16 08:33:34 -0300

    ✨Adicionados alguns error handlers de user e nick

---
+++ server/src/helpers/errorHandlers.js
@@ -0,0 +1,22 @@
+import { serverName } from "../server.js"
+import sendMessageToUser from "../helpers/sendMessageToUser.js"
+
+function notRegisteredErrorHandler(socket) {
+    const errorCode = 451;
+    const message = "You have not registered";
+    sendMessageToUser(serverName, socket, errorCode, [message])
+}
+
+function noNickNameGivenErrorHandler(socket) {
+    const errorCode = 431;
+    const message = "No nickname given";
+    sendMessageToUser(serverName, socket, errorCode, [message]);
+}
+
+function nickNameInUseErrorHandler(socket, nick) {
+    const errorCode = 433;
+    const message = "Nickname is already in use";
+    sendMessageToUser(serverName, socket, errorCode, [nick, message])
+}
+
+export { notRegisteredErrorHandler, noNickNameGivenErrorHandler, nickNameInUseErrorHandler };
