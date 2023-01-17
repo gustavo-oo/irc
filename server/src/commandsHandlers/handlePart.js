34a93d4d724e1c7086a940105bfba941c8c95352
David Mendes <davidgmendes@hotmail.com>
2023-01-17 00:07:42 -0300

    :goal_net: Handle not enough parameters error for part

--- server/src/commandsHandlers/handlePart.js
+++ server/src/commandsHandlers/handlePart.js
@@ -3,25 +3,32 @@ import sendMessageToChannel from "../helpers/sendMessageToChannel.js";
 import sendMessageToUser from "../helpers/sendMessageToUser.js"
 import { serverName } from "../server.js";
 
-export default function handlePart(socket, channelsNames, partMessage) {
+export default function handlePart(socket, channelsNames) {
   const defaultMessage = "Tchau";
+
+  if(!channelsNames){
+    sendMessageToUser(
+      serverName, socket, 461, ["PART", "Not enough parameters"]
+    );
+    return;
+  }
 
   channelsNames.split(',').map((channelName) => {
     if (getUserChannel(socket) === channelName) {
       sendMessageToChannel(
         socket,
         "PART",
-        partMessage || defaultMessage,
+        defaultMessage,
         getUserChannel(socket)
       );
       removeUserFromChannel(socket);
     } else if(channelExists(channelName)) {
       sendMessageToUser(
-        serverName, socket, 442, [`${channelName} :You're not on that channel`]
+        serverName, socket, 442, [channelName, "You're not on that channel"]
       );
     } else {
       sendMessageToUser(
-        serverName, socket, 402, [`${channelName} :No such channel`]
+        serverName, socket, 402, [channelName, "No such channel"]
       );
     }
   })
