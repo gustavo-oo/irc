87b807e3cb31599eacc818333df207588e56a299
David Mendes <davidgmendes@hotmail.com>
2023-01-17 10:55:48 -0300

    :goal_net: no such channel error for handle join

--- server/src/commandsHandlers/handleJoin.js
+++ server/src/commandsHandlers/handleJoin.js
@@ -2,11 +2,20 @@ import { addUserToChannel, usersInChannel } from "../store.js";
 import sendMessageToChannel from "../helpers/sendMessageToChannel.js";
 import sendMessageToUser from "../helpers/sendMessageToUser.js";
 import { serverName } from "../server.js";
+import { needMoreParamsErrorHandler, noSuchChannelErrorHandler } from "../helpers/errorHandlers.js";
 
 export default function handleJoin(socket, channelName) {
 
   if(!channelName){
     needMoreParamsErrorHandler(socket, "JOIN");
+    return;
+  }
+
+  if(
+    (channelName[0] !== '&' && channelName[0] !== '#') || channelName.length > 9 ||
+    channelName.includes(" ") || channelName.includes(",")
+  ) {
+    noSuchChannelErrorHandler(socket, channelName);
     return;
   }
 
