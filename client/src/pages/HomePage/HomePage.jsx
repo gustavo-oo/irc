9e59df79135bff687235cc0fc157ab52224d6874
Júlia Baba <julia.baba@grupoorion.eng.br>
2023-01-17 22:50:58 -0300

    ✅ states

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,14 @@
 import MsnIcon from "../../components/MsnIcon/MsnIcon";
 import "./HomePage.css";
+import { useState } from "react";
 export default function HomePage() {
+  const [nick, setNick] = useState("");
+  const [username, setUserName] = useState("");
+  const [fullname, setFullName] = useState("");
+
+  function onSubmit() {
+    return;
+  }
   return (
     <div className="wholePage">
       <div className="header">
@@ -13,12 +21,26 @@ export default function HomePage() {
         </div>
         <div className="inputConteiner">
           <label for="fname">Apelido:</label>
-          <input placeholder="Nick"></input>
+          <input
+            placeholder="Nick"
+            value={nick}
+            onChange={(event) => setNick(event.target.value)}
+          ></input>
           <label for="fname">Nome do Usuário:</label>
-          <input placeholder="Username"></input>
+          <input
+            placeholder="Username"
+            value={username}
+            onChange={(event) => setUserName(event.target.value)}
+          ></input>
           <label for="fname">Nome Completo:</label>
-          <input placeholder="Full Name"></input>
-          <button className="enterButton">Entrar</button>
+          <input
+            placeholder="Full Name"
+            value={fullname}
+            onChange={(event) => setFullName(event.target.value)}
+          ></input>
+          <button className="enterButton" onClick={onSubmit}>
+            Entrar
+          </button>
         </div>
       </div>
     </div>
