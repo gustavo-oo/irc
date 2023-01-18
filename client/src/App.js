4655df7ac9df66293ba31cd87bb842964993c5ff
Gustavo <gustavopchaves@hotmail.com>
2023-01-17 23:48:42 -0300

    ✨ Função de envio de mensagem, sem tratamento de respostas

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,53 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import processMessage from './helpers/processChatMessage';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+const App = ({ socket }) => {
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      socket.emit('message', processMessage(message));
+      setMessage('');
+    };
+    
+    useEffect(() => {
+      if (socket) {
+        socket.on('connect', () => {
+          console.log('Connected to the server');
+        });
+    
+        socket.on('end', () => {
+            console.log('Disconnected from the server');
+        });
+    
+        socket.on('message', (data) => {
+            console.log('Received data:', data);
+        });
+      }
+      return () => {
+        if (socket) {
+          socket.off('message');
+        }
+      };
+    }, [socket]);
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    onKeyPress={(e) => {
+                        if (e.key === 'Enter') {
+                            handleSubmit(e);
+                        }
+                    }}
+                />
+                <button type='submit'>Send</button>
+            </form>
+        </div>
+    );
+};
 
 export default App;
--- server/src/helpers/getHostName.js
+++ server/src/helpers/getHostName.js
@@ -1,14 +1,10 @@
-import getClientId from "./getClientId.js";
-
 import { reverse } from 'dns';
 
 export default async function getHostName(socket) {
-    const clientId = getClientId(socket);
-
     return new Promise((resolve, reject) => {
-        reverse(clientId, (error, hostnames) => {
+        reverse(socket.remoteAddress, (error, hostnames) => {
             if (error || hostnames.length === 0) {
-                resolve(clientId);
+                resolve(`${socket.remoteAddress}:${socket.remotePort}`);
                 return;
             }
             
