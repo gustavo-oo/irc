2da18d24025cceb4dbc7097d33f4419e0d947a77
Júlia Baba <julia.baba@grupoorion.eng.br>
2023-01-18 11:49:14 -0300

    ✅ icon fixed

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import HomePage from "./pages/HomePage/HomePage";
 
 function App({ socket }) {
   const [message, setMessage] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
 
   const pages = [<HomePage />, <ChatScreen />];
 
